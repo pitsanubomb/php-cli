23be6895e9275828926afb1339f11d86e2a100fc
cgloveut <pitsanubomb@gmail.com>
2019-01-26 01:10:15 +0700

    Add entities and connect db :sparkles:

---
+++ config/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+$paths = array(__DIR__."/entities");
+$isDevMode = true;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => 'password',
+    'dbname'   => 'property',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
---
+++ entities/geodata.php
@@ -0,0 +1,105 @@
+<?php
+    /**
+    * @Entity @Table(name="geodata")
+    */
+    class Geodata
+    {
+        /** @Id @Column(type="integer") @GeneratedValue */
+        protected $id;
+        /** @Column(type="string") */
+        protected $country;
+        /** @Column(type="string") */
+        protected $houseaddress;
+        /** @Column(type="string") */
+        protected $street;
+        /** @Column(type="int") */
+        protected $postcodeid;
+        /** @Column(type="int") */
+        protected $townid;
+        /** @Column(type="int") */
+        protected $regionid;
+        /** @Column(type="float") */
+        protected $latitude;
+        /** @Column(type="float") */
+        protected $longtitude;
+        /** @Column(type="int") */
+        protected $zoneid;
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function setCountry($country) {
+            $this->country = $country;
+        }
+
+        public function getCountry(){
+            return $this->country;
+        }
+
+        public function setHouseAddress($houseaddress) {
+            $this->houseaddress = $houseaddress
+        }
+
+        public function getHouseAddress() {
+            return $this->houseaddress;
+        }
+
+        public function setStreet($street) {
+            $this->street = $street
+        }
+
+        public function getStreet() {
+            return $this->street;
+        }
+
+        public function setPostCodeId($postcodeid) {
+            $this->postcodeid = $postcodeid
+        }
+
+        public function getPostCodeId() {
+            return $this->postcodeid;
+        }
+
+        public function setTownId($townid) {
+            $this->townid = $townid
+        }
+
+        public function getTownId() {
+            return $this->townid;
+        }
+
+        public function setRegionId($regionid) {
+            $this->regionid = $regionid
+        }
+
+        public function getRegionId() {
+            return $this->regionid;
+        }
+
+        public function setLattiTude($latitude) {
+            $this->lattitude = $latitude;
+        }
+
+        public function getLattiTude() {
+            return $this->lattitude;
+        }
+
+        public function setLongtiTude($longtitude) {
+            $this->longtitude = $longtitude;
+        }
+
+        public function getLongtiTude() {
+            return $this->longtitude;
+        }
+
+        public function setZonneId($zondeid) {
+            $this->zoneid = $zondeid;
+        }
+
+        public function getZoneId()
+        {
+            return $this->zoneid
+        }        
+    }
+?>
